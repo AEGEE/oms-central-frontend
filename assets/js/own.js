7639c2a5ec891f94d108f14f9b8dd8b650faed00
Nico Westerbeck <Nico.Westerbeck@mailbox.tu-dresden.de>
2018-04-27 16:29:58 +0200

    Added basic animations

--- assets/js/own.js
+++ assets/js/own.js
@@ -79,7 +79,9 @@ const infiniteScroll = ($http, vm, url, paramInjector) => {
   vm.resetData = () => {
     vm.infiniteScroll.block = false;
     vm.infiniteScroll.busy = false;
-    vm.infiniteScroll.data = [];
+    if(!vm.infiniteScroll.data)
+      vm.infiniteScroll.data = [];
+    vm.infiniteScroll.resetOnNextFetch = true;
     vm.infiniteScroll.page = 0;
     return vm.loadNextPage();
   }
@@ -95,24 +97,60 @@ const infiniteScroll = ($http, vm, url, paramInjector) => {
     };
     if(paramInjector)
       params = paramInjector(params);
+
+    // Use sendParams in case you want to strip empty params
+    /*
     var sendParams = {};
     for(var key in params) {
       if(params.hasOwnProperty(key) && params[key])
         sendParams[key] = params[key];
     }
-
+    */
 
     return $http({
       url: url,
       method: 'GET',
-      params: sendParams
+      params: params
     }).then((response) => {
       if(localRaceCounter == vm.infiniteScroll.raceCounter) {
         vm.infiniteScroll.busy = false;
+        // If we have data, add that to the list
         if(response.data.data.length > 0) {
           vm.infiniteScroll.page++;
-          vm.infiniteScroll.data.push.apply(vm.infiniteScroll.data, response.data.data);
-          vm.infiniteScroll.block = false;
+
+          // To preserve nice angular animations try to do a "soft" reset where we only remove those elements that are not in the response
+          if(vm.infiniteScroll.resetOnNextFetch == true) {
+            vm.infiniteScroll.resetOnNextFetch = false;
+            var new_data = response.data.data.slice();
+            const isPartnerElement = function(elem1, elem2) {return elem1.id == elem2.id;}
+            // Loop through the existing data and try to find partner elements in the new data
+            for(let i=0; i<vm.infiniteScroll.data.length; i++) {
+              let idx = new_data.findIndex(isPartnerElement.bind(null, vm.infiniteScroll.data[i]));
+              // In case we found no partner element, remove that one
+              if(idx == -1) {
+                vm.infiniteScroll.data.splice(i, 1);
+                i--;
+              } 
+              // In case we did find a partner elements, remove that from the input data to not add it again at the end
+              else {
+                new_data.splice(idx, 1);
+              }
+            }
+            // Add remaining items which we haven't added yet
+            Array.prototype.push.apply(vm.infiniteScroll.data, new_data);
+          }
+          // If it was a load of an additional page just add data
+          else 
+            Array.prototype.push.apply(vm.infiniteScroll.data, response.data.data);
+          
+          // If we got less than requested results, do not ask for more
+          if(response.data.data.length >= vm.infiniteScroll.pageSize)
+            vm.infiniteScroll.block = false;
+        } 
+        // In case we got no results, don't forget to reset
+        else if(vm.infiniteScroll.resetOnNextFetch == true) {
+          vm.infiniteScroll.resetOnNextFetch = false;
+          vm.infiniteScroll.data = [];
         }
       }
     }).catch((error) => {
